569dc52ad1c20ab9021e24712165c78eb2b94331
deraadt <>
1996-09-03 10:53:37 +0000

    proper types

--- src/lib/libc/net/rcmd.c
+++ src/lib/libc/net/rcmd.c
@@ -34,7 +34,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$OpenBSD: rcmd.c,v 1.15 1996/09/01 18:31:29 deraadt Exp $";
+static char *rcsid = "$OpenBSD: rcmd.c,v 1.16 1996/09/02 21:26:09 millert Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
@@ -54,6 +54,7 @@ static char *rcsid = "$OpenBSD: rcmd.c,v 1.15 1996/09/01 18:31:29 deraadt Exp $"
 #include <ctype.h>
 #include <string.h>
 #include <syslog.h>
+#include <stdlib.h>
 
 int	__ivaliduser __P((FILE *, u_long, const char *, const char *));
 static int __icheckhost __P((u_int32_t, const char *));
