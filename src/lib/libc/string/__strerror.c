78abea134658142d7d9cbca14822f8ca91b9a7e7
tholo <>
1996-09-15 09:31:53 +0000

    Remove dead code Remove unused variables Silence some warnings lint(1) is your friend

--- src/lib/libc/string/__strerror.c
+++ src/lib/libc/string/__strerror.c
@@ -32,7 +32,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$OpenBSD: __strerror.c,v 1.2 1996/01/29 02:04:11 deraadt Exp $";
+static char *rcsid = "$OpenBSD: __strerror.c,v 1.3 1996/08/19 08:33:55 tholo Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #ifdef NLS
@@ -64,7 +64,7 @@ __strerror(num, buf)
 	register unsigned int errnum;
 
 #ifdef NLS
-	nl_catd catd ;
+	nl_catd catd;
 	catd = catopen("libc", 0);
 #endif
 
