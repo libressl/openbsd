c9952142a4227dc1e164ae5bbc7bb1c36e02c244
deraadt <>
1998-02-10 02:19:48 +0000

    move proto to unistd.h

--- src/lib/libc/string/swab.c
+++ src/lib/libc/string/swab.c
@@ -35,10 +35,10 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$OpenBSD: swab.c,v 1.1.1.1 1995/10/18 08:42:23 deraadt Exp $";
+static char *rcsid = "$OpenBSD: swab.c,v 1.2 1996/08/19 08:34:30 tholo Exp $";
 #endif /* LIBC_SCCS and not lint */
 
-#include <string.h>
+#include <unistd.h>
 
 void
 swab(from, to, len)
