c1cd143ac88d92a612c53be25a09de84b39074b5
d <>
1998-11-20 11:18:51 +0000

    Add thread-safety to libc, so that libc_r will build (on i386 at least). All POSIX libc api now there (to P1003.1c/D10)   (more md stuff is needed for other libc/arch/*)   (setlogin is no longer a special syscall) Add -pthread option to gcc (that makes it use -lc_r and -D_POSIX_THREADS). Doc some re-entrant routines Add libc_r to intro(3) dig() uses some libc srcs and an extra -I was needed there. Add more md stuff to libc_r. Update includes for the pthreads api Update libc_r TODO

--- src/lib/libc/include/spinlock.h
+++ src/lib/libc/include/spinlock.h
@@ -1,5 +1,5 @@
-/*-
- * Copyright (c) 1990 The Regents of the University of California.
+/*
+ * Copyright (c) 1998 John Birrell <jb@cimlogic.com.au>.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -12,13 +12,12 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ *	This product includes software developed by John Birrell.
+ * 4. Neither the name of the author nor the names of any co-contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * THIS SOFTWARE IS PROVIDED BY JOHN BIRRELL AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
@@ -29,34 +28,46 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
+ *
+ * $Id$
+ * $OpenBSD$
+ *
+ * Lock definitions used in both libc and libpthread.
+ *
  */
 
-#if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$OpenBSD: rand.c,v 1.2 1996/08/19 08:33:44 tholo Exp $";
-#endif /* LIBC_SCCS and not lint */
-
+#ifndef _SPINLOCK_H_
+#define _SPINLOCK_H_
+#include <sys/cdefs.h>
 #include <sys/types.h>
-#include <stdlib.h>
 
-static u_long next = 1;
+/*
+ * Lock structure with room for debugging information.
+ */
+typedef struct {
+	volatile register_t	access_lock;
+	volatile long	lock_owner;
+	volatile const char * fname;
+	volatile int	lineno;
+} spinlock_t;
 
-int
-rand_r(seed)
-u_int *seed;
-{
-	*seed = *seed * 1103515245 + 12345;
-	return (u_int)(*seed / 65536) % ((u_int)RAND_MAX + 1);
-}
+#define	_SPINLOCK_INITIALIZER	{ 0, 0, 0, 0 }
 
-int
-rand()
-{
-	return rand_r(&next);
-}
+#define _SPINUNLOCK(_lck)	(_lck)->access_lock = 0
+#ifdef	_LOCK_DEBUG
+#define	_SPINLOCK(_lck)		_spinlock_debug(_lck, __FILE__, __LINE__)
+#else
+#define	_SPINLOCK(_lck)		_spinlock(_lck)
+#endif
 
-void
-srand(seed)
-u_int seed;
-{
-	next = seed;
-}
+/*
+ * Thread function prototype definitions:
+ */
+__BEGIN_DECLS
+register_t	_atomic_lock __P((volatile register_t *));
+register_t	_thread_slow_atomic_lock __P((volatile register_t *));
+void	_spinlock __P((spinlock_t *));
+void	_spinlock_debug __P((spinlock_t *, const char *, int));
+__END_DECLS
+
+#endif /* _SPINLOCK_H_ */
