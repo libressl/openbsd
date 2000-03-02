a58d1dc5a1a673a0c41746780dedc3c1e452ccd1
todd <>
2000-03-02 00:29:51 +0000

    $OpenBSD$

--- src/lib/libcrypto/des/supp.c
+++ src/lib/libcrypto/des/supp.c
@@ -1,3 +1,4 @@
+/*	$OpenBSD: supp.c,v 1.2 1999/09/29 04:36:01 beck Exp $	*/
 /* crypto/des/supp.c */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
@@ -87,7 +88,6 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id$
  */
 
 #include <stdio.h>
