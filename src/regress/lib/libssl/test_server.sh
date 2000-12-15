9d5e977da5ec8b042075bab2e6dfe36773b53842
cvs2svn <admin@example.com>
2000-12-15 02:58:50 +0000

    This commit was manufactured by cvs2git to create branch 'unlabeled-1.3.4'.

--- src/regress/lib/libssl/test_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo This starts a tls1 mode server using the DSA certificate in ./server.pem
-echo Run ./testclient.sh in another window and type at it, you should 
-echo see the results of the ssl negotiation, and stuff you type in the client
-echo should echo in this window
-echo
-echo
-/usr/sbin/openssl s_server -tls1 -key testdsa.key -cert testdsa.pem
