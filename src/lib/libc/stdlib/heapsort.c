db5fd358a198e325935348673e86c3902927f636
millert <>
2002-02-16 21:27:32 +0000

    Part one of userland __P removal.  Done with a simple regexp with some minor hand editing to make comments line up correctly.  Another pass is forthcoming that handles the cases that could not be done automatically.

--- src/lib/libc/stdlib/heapsort.c
+++ src/lib/libc/stdlib/heapsort.c
@@ -35,7 +35,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$OpenBSD: heapsort.c,v 1.1.1.1 1995/10/18 08:42:17 deraadt Exp $";
+static char *rcsid = "$OpenBSD: heapsort.c,v 1.2 1996/08/19 08:33:32 tholo Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -141,7 +141,7 @@ int
 heapsort(vbase, nmemb, size, compar)
 	void *vbase;
 	size_t nmemb, size;
-	int (*compar) __P((const void *, const void *));
+	int (*compar)(const void *, const void *);
 {
 	register int cnt, i, j, l;
 	register char tmp, *tmp1, *tmp2;
