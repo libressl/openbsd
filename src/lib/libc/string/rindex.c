84572a5a0c804c4a3d54a2134f999fe40b792a3d
millert <>
2002-07-24 04:16:01 +0000

    Convert to ANSI function headers and make 'ch' argument int, not char. Noticed by deraadt@

--- src/lib/libc/string/rindex.c
+++ src/lib/libc/string/rindex.c
@@ -32,18 +32,17 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$OpenBSD: rindex.c,v 1.1.1.1 1995/10/18 08:42:21 deraadt Exp $";
+static char *rcsid = "$OpenBSD: rindex.c,v 1.2 1996/08/19 08:34:08 tholo Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <string.h>
 
 char *
 #ifdef STRRCHR
-strrchr(p, ch)
+strrchr(const char *p, int ch)
 #else
-rindex(p, ch)
+rindex(const char *p, int ch)
 #endif
-	register const char *p, ch;
 {
 	register char *save;
 
