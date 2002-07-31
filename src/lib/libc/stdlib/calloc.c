4d623ca1130c6cc4f62c85f6107db6da347847d8
deraadt <>
2002-07-31 09:19:04 +0000

    permit calloc(0, N) and calloc(N, 0) -- malloc(0) does the right thing; markus miod ok

--- src/lib/libc/stdlib/calloc.c
+++ src/lib/libc/stdlib/calloc.c
@@ -32,7 +32,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$OpenBSD: calloc.c,v 1.5 1999/11/10 20:12:31 millert Exp $";
+static char *rcsid = "$OpenBSD: calloc.c,v 1.6 2002/07/30 00:11:07 deraadt Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <stdlib.h>
@@ -47,7 +47,7 @@ calloc(num, size)
 {
 	register void *p;
 
-	if (SIZE_T_MAX / num < size) {
+	if (num && size && SIZE_T_MAX / num < size) {
 		errno = ENOMEM;
 		return NULL;
 	}
