4cec29df4ad2feb921d8037ec8ffce0fef09561c
david <>
2003-08-02 01:24:36 +0000

    add missing includes ok deraadt@ tedu@

--- src/regress/lib/libc/alloca/alloca.c
+++ src/regress/lib/libc/alloca/alloca.c
@@ -1,8 +1,9 @@
-/*	$OpenBSD: alloca.c,v 1.3 2003/07/31 03:23:41 mickey Exp $	*/
+/*	$OpenBSD: alloca.c,v 1.4 2003/07/31 21:48:02 deraadt Exp $	*/
 
 /*	Written by Michael Shalayeff, 2003, Public Domain.	*/
 
 #include <stdio.h>
+#include <string.h>
 
 int
 main(int argc, char *argv[])
