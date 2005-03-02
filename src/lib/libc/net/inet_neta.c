d4e2598ffbf22b6c4009c051d005575d3757380f
millert <>
2005-03-02 12:27:26 +0000

    Consistent #if protection of rcs ids; Francois Perrad

--- src/lib/libc/net/inet_neta.c
+++ src/lib/libc/net/inet_neta.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: inet_neta.c,v 1.3 2002/05/24 21:22:37 deraadt Exp $	*/
+/*	$OpenBSD: inet_neta.c,v 1.4 2002/08/19 03:01:54 itojun Exp $	*/
 
 /*
  * Copyright (c) 1996 by Internet Software Consortium.
@@ -18,11 +18,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-#if 0
-static const char rcsid[] = "$Id: inet_neta.c,v 1.4 2002-08-19 03:01:54 itojun Exp $";
-#else
-static const char rcsid[] = "$OpenBSD: inet_neta.c,v 1.3 2002/05/24 21:22:37 deraadt Exp $";
-#endif
+static const char rcsid[] = "$OpenBSD: inet_neta.c,v 1.4 2002/08/19 03:01:54 itojun Exp $";
 #endif
 
 #include <sys/types.h>
