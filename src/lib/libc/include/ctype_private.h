7b8ff606e1bafc2847c5566102f1e4475d30895d
espie <>
2005-08-08 05:53:01 +0000

    activate LC_CTYPE for 8 bits locale. Make sure tolower/toupper use the whole 8 bits.
    okay deraadt@
    thanks to everyone who tested

---
+++ src/lib/libc/include/ctype_private.h
@@ -0,0 +1,7 @@
+/* $OpenBSD$ */
+/* Written by Marc Espie, public domain */
+#define CTYPE_NUM_CHARS       256
+extern const char _C_ctype_[];
+extern const short _C_toupper_[];
+extern const short _C_tolower_[];
+
