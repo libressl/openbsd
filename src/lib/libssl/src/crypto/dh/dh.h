1d20264fcecb551f6a52f5ef0d92b0a89bd89742
brad <>
2006-10-05 18:42:29 +0000

    MFC: Fix by pvalchev@
    openssl security fixes, diff from markus@, ok & "commit it" djm@
    http://www.openssl.org/news/secadv_20060928.txt for more

--- src/lib/libssl/src/crypto/dh/dh.h
+++ src/lib/libssl/src/crypto/dh/dh.h
@@ -70,6 +70,8 @@
 #include <openssl/crypto.h>
 #include <openssl/ossl_typ.h>
 	
+#define OPENSSL_DH_MAX_MODULUS_BITS	10000
+
 #define DH_FLAG_CACHE_MONT_P	0x01
 
 #ifdef  __cplusplus
@@ -200,6 +202,7 @@ void ERR_load_DH_strings(void);
 /* Reason codes. */
 #define DH_R_BAD_GENERATOR				 101
 #define DH_R_NO_PRIVATE_VALUE				 100
+#define DH_R_MODULUS_TOO_LARGE				 103
 
 #ifdef  __cplusplus
 }
