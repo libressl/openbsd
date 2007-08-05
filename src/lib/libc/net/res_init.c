ab282b7662ba68f86d4f44ba31882967f3545346
ray <>
2007-08-05 16:11:09 +0000

    /*FALLTHRU*/ -> /* FALLTHROUGH */

--- src/lib/libc/net/res_init.c
+++ src/lib/libc/net/res_init.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: res_init.c,v 1.34 2005/12/22 06:52:11 tedu Exp $	*/
+/*	$OpenBSD: res_init.c,v 1.35 2006/09/23 04:56:43 ray Exp $	*/
 
 /*
  * ++Copyright++ 1985, 1989, 1993
@@ -506,7 +506,7 @@ _res_init(int usercall)
 				    m = -1;
 				    break;
 				}
-				/*FALLTHRU*/
+				/* FALLTHROUGH */
 			    default:
 				m = sizeof(struct in6_addr) * NBBY;
 				break;
