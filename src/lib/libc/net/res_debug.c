be174335f7767155ab4154a558241cd28bfe1a96
jakob <>
2007-10-11 18:36:41 +0000

    use RRSIG instead of SIG for DNSSEC. ok djm@

--- src/lib/libc/net/res_debug.c
+++ src/lib/libc/net/res_debug.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: res_debug.c,v 1.20 2005/03/30 02:58:28 tedu Exp $	*/
+/*	$OpenBSD: res_debug.c,v 1.21 2005/08/06 20:30:04 espie Exp $	*/
 
 /*
  * ++Copyright++ 1985, 1990, 1993
@@ -751,6 +751,7 @@ __p_rr(const u_char *cp, const u_char *msg, FILE *file)
 		break;
 
 	case T_SIG:
+	case T_RRSIG:
 	        type = _getshort((u_char*)cp);
 		cp += INT16SZ;
 		fprintf(file, " %s", p_type(type));
