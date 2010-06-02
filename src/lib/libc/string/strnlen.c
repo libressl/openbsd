eb70496c5354f740a9b715eb16141e51e92c97d5
millert <>
2010-06-02 12:58:12 +0000

    Avoid using and end pointer since strnlen(string, -1) is legal and would otherwise result in overflowing the end pointer and cause strnlen() to return 0.  OK sthen@

--- src/lib/libc/string/strnlen.c
+++ src/lib/libc/string/strnlen.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: strnlen.c,v 1.1 2010/05/18 22:24:55 tedu Exp $	*/
+/*	$OpenBSD: strnlen.c,v 1.2 2010/05/21 06:57:45 chl Exp $	*/
 
 /*
  * Copyright (c) 2010 Todd C. Miller <Todd.Miller@courtesan.com>
@@ -23,10 +23,9 @@
 size_t
 strnlen(const char *str, size_t maxlen)
 {
-	const char *cp, *ep;
+	const char *cp;
 
-	ep = str + maxlen;
-	for (cp = str; cp < ep && *cp != '\0'; cp++)
+	for (cp = str; maxlen != 0 && *cp != '\0'; cp++, maxlen--)
 		;
 
 	return (size_t)(cp - str);
