ddb6b22abd29c481b0adcbab17c4ead3bde70fa2
djm <>
2010-10-01 23:33:22 +0000

    fix -Wall due to API change

--- src/lib/libssl/src/crypto/evp/e_acss.c
+++ src/lib/libssl/src/crypto/evp/e_acss.c
@@ -32,7 +32,7 @@ typedef struct {
 static int acss_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key,
 		const unsigned char *iv, int enc);
 static int acss_ciph(EVP_CIPHER_CTX *ctx, unsigned char *out,
-		const unsigned char *in, unsigned int inl);
+		const unsigned char *in, size_t inl);
 static int acss_ctrl(EVP_CIPHER_CTX *ctx, int type, int arg, void *ptr);
 static const EVP_CIPHER acss_cipher = {
 	NID_undef,
@@ -64,7 +64,7 @@ acss_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key,
 
 static int
 acss_ciph(EVP_CIPHER_CTX *ctx, unsigned char *out, const unsigned char *in,
-		unsigned int inl)
+		size_t inl)
 {
 	acss(&data(ctx)->ks,inl,in,out);
 	return 1;
