9ee4cfe15cfdc989aa4e6a94c943d73971f77a96
deraadt <>
2012-12-05 23:20:07 +0000

    Remove excessive sys/cdefs.h inclusion ok guenther millert kettenis

--- src/regress/lib/libc/regex/t_exhaust.c
+++ src/regress/lib/libc/regex/t_exhaust.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: t_exhaust.c,v 1.1 2011/11/05 15:01:37 otto Exp $	*/
+/*	$OpenBSD: t_exhaust.c,v 1.2 2011/11/06 15:47:07 otto Exp $	*/
 /*	$NetBSD: t_exhaust.c,v 1.2 2011/10/21 00:41:34 christos Exp $	*/
 
 /*-
@@ -36,8 +36,6 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-
-#include <sys/cdefs.h>
 
 #include <stdio.h>
 #include <regex.h>
