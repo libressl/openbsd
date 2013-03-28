a47fd7bd68c53de0dcf0b08df2fae59a79891820
martynas <>
2013-03-28 18:09:38 +0000

    Switch libc and libm to use strong aliases rather than weak aliases where appropriate.  Among other things makes the symbols consistent across all architectures (notably where ldbl mantissa is 53 bits).
    While at it, kill unused LINTLIBRARY/PROTOLIB1 cruft which was there
    to trick lint into recording the right prototypes for aliased
    functions.  Most of the work done at the awesome n2k13 hackathon.
    
    Agreed by kettenis@, guenther@, matthew@.

--- src/lib/libc/stdlib/strtoll.c
+++ src/lib/libc/stdlib/strtoll.c
@@ -1,4 +1,4 @@
-/* $OpenBSD$ */
+/* $OpenBSD: strtoll.c,v 1.6 2005/11/10 10:00:17 espie Exp $ */
 /*-
  * Copyright (c) 1992 The Regents of the University of California.
  * All rights reserved.
@@ -141,13 +141,4 @@ strtoll(const char *nptr, char **endptr, int base)
 	return (acc);
 }
 
-#ifdef __weak_alias
-__weak_alias(strtoq, strtoll);
-#else
-quad_t
-strtoq(const char *nptr, char **endptr, int base)
-{
-
-        return ((quad_t)strtoll(nptr, endptr, base));
-}
-#endif
+__strong_alias(strtoq, strtoll);
