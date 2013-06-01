1aa67b344b8581052651568d6b30a7f29eab5400
stsp <>
2013-06-01 21:26:18 +0000

    Fix catopen(3) for UTF-8 locales and update the implementation to POSIX-2008.
    Catalogs had an arbitrary character set which was used regardless of locale.
    Add UTF-8-encoded catalogs for libc error messages, and rename existing ones
    to indicate their character sets. catopen(3) now chooses a catalog which
    matches the locale's encoding, if available.
    
    help & ok matthew, man page bits ok jmc, input from naddy and bluhm

--- src/lib/libc/string/strerror_r.c
+++ src/lib/libc/string/strerror_r.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: strerror_r.c,v 1.6 2005/08/08 08:05:37 espie Exp $ */
+/* $OpenBSD: strerror_r.c,v 1.7 2007/03/01 16:29:09 bluhm Exp $ */
 /* Public Domain <marc@snafu.org> */
 
 #ifdef NLS
@@ -75,7 +75,7 @@ __num2string(int num, int sign, int setid, char *buf, size_t buflen,
 
 #ifdef NLS
 	nl_catd catd;
-	catd = catopen("libc", 0);
+	catd = catopen("libc", NL_CAT_LOCALE);
 #endif
 
 	if (0 <= num && num < max) {
