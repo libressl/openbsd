a85dcc9f8c8573f19f2a868a1f9de9b52af1e5f3
schwarze <>
2014-04-03 16:18:11 +0000

    Update Copyright notice; ok otto@ beck@ deraadt@. This is merely a by-product of figuring out the amount of phk@ code contained herein; i'm not planning to hack on this file.

--- src/lib/libc/stdlib/malloc.c
+++ src/lib/libc/stdlib/malloc.c
@@ -1,6 +1,8 @@
-/*	$OpenBSD: malloc.c,v 1.150 2013/11/12 06:57:54 deraadt Exp $	*/
+/*	$OpenBSD: malloc.c,v 1.151 2014/03/25 17:00:02 beck Exp $	*/
 /*
- * Copyright (c) 2008 Otto Moerbeek <otto@drijf.net>
+ * Copyright (c) 2008, 2010, 2011 Otto Moerbeek <otto@drijf.net>
+ * Copyright (c) 2012 Matthew Dempsky <matthew@openbsd.org>
+ * Copyright (c) 2008 Damien Miller <djm@openbsd.org>
  * Copyright (c) 2000 Poul-Henning Kamp <phk@FreeBSD.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
