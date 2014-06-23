834435c7d0ad5ebe7b39caa4a1ba41d8918744ce
beck <>
2014-06-23 03:32:57 +0000

    unbreak - main needs to be extern in here somewhere.

--- src/lib/libcrypto/crypto/getentropy_linux.c
+++ src/lib/libcrypto/crypto/getentropy_linux.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: getentropy_linux.c,v 1.5 2014/06/21 02:08:34 deraadt Exp $	*/
+/*	$OpenBSD: getentropy_linux.c,v 1.6 2014/06/21 21:44:36 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2014 Theo de Raadt <deraadt@openbsd.org>
@@ -61,6 +61,7 @@
 
 int	getentropy(void *buf, size_t len);
 
+extern int main(int, char *argv[]);
 static int gotdata(char *buf, size_t len);
 static int getentropy_urandom(void *buf, size_t len);
 static int getentropy_sysctl(void *buf, size_t len);
