df78bd9aacd3582d7a79761b17d0558870ebd472
jsing <>
2014-07-12 19:31:21 +0000

    In openssl_startup(), call SSL_library_init() and SSL_load_error_strings(). This allows us to remove the ERR_load_crypto_strings() call, along with the various SSL_load_error_strings() and OpenSSL_add_ssl_algorithms() calls scattered around the place.
    ok beck@

--- src/lib/libssl/src/apps/errstr.c
+++ src/lib/libssl/src/apps/errstr.c
@@ -1,4 +1,4 @@
-/* $OpenBSD$ */
+/* $OpenBSD: errstr.c,v 1.17 2014/06/12 15:49:27 deraadt Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -75,8 +75,6 @@ errstr_main(int argc, char **argv)
 	int i, ret = 0;
 	char buf[256];
 	unsigned long l;
-
-	SSL_load_error_strings();
 
 	if ((argc > 1) && (strcmp(argv[1], "-stats") == 0)) {
 		BIO *out = NULL;
