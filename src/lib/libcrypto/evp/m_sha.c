95be4c091c3d1ae7b3077a97df3e1634f0e27a3c
miod <>
2014-07-13 09:30:02 +0000

    No need to include evp_locl.h in there.

--- src/lib/libcrypto/evp/m_sha.c
+++ src/lib/libcrypto/evp/m_sha.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: m_sha.c,v 1.14 2014/07/10 22:45:57 jsing Exp $ */
+/* $OpenBSD: m_sha.c,v 1.15 2014/07/11 08:44:48 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -69,8 +69,6 @@
 #ifndef OPENSSL_NO_RSA
 #include <openssl/rsa.h>
 #endif
-
-#include "evp_locl.h"
 
 static int
 init(EVP_MD_CTX *ctx)
