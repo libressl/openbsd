613e889480917fa6546511b8f5fea42632d48ed3
beck <>
2014-07-13 10:27:22 +0000

    Take out __bounded__ in the include files we use it in when not on OpenBSD. while we can take it out in portable at compile time, it is still a problem when we install this header file on a system that doesn't support __bounded__ if this is unguarded. ok miod@ bcook@

--- src/lib/libcrypto/bio/bio.h
+++ src/lib/libcrypto/bio/bio.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: bio.h,v 1.23 2014/06/12 15:49:28 deraadt Exp $ */
+/* $OpenBSD: bio.h,v 1.24 2014/06/22 14:41:10 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -58,7 +58,9 @@
 
 #ifndef HEADER_BIO_H
 #define HEADER_BIO_H
-
+#if !defined(HAVE_ATTRIBUTE__BOUNDED__) || !defined(__OpenBSD__)
+#define __bounded__(x, y, z)
+#endif
 #include <openssl/opensslconf.h>
 
 # include <stdio.h>
