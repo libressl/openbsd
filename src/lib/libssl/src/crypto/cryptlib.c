e2ae7953bcfa1144a0b994f7c2b5dfe59905242c
beck <>
2014-07-22 02:21:20 +0000

    Kill a bunch more BUF_strdup's - these are converted to have a check for NULL before an intrinsic strdup. ok miod@

--- src/lib/libssl/src/crypto/cryptlib.c
+++ src/lib/libssl/src/crypto/cryptlib.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: cryptlib.c,v 1.31 2014/07/10 22:45:56 jsing Exp $ */
+/* $OpenBSD: cryptlib.c,v 1.32 2014/07/11 08:44:47 jsing Exp $ */
 /* ====================================================================
  * Copyright (c) 1998-2006 The OpenSSL Project.  All rights reserved.
  *
@@ -210,7 +210,7 @@ CRYPTO_get_new_lockid(char *name)
 		CRYPTOerr(CRYPTO_F_CRYPTO_GET_NEW_LOCKID, ERR_R_MALLOC_FAILURE);
 		return (0);
 	}
-	if ((str = BUF_strdup(name)) == NULL) {
+	if (name == NULL || (str = strdup(name)) == NULL) {
 		CRYPTOerr(CRYPTO_F_CRYPTO_GET_NEW_LOCKID, ERR_R_MALLOC_FAILURE);
 		return (0);
 	}
