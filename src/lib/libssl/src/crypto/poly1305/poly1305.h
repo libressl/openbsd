8e7ff3504ff08e3840499d39ae691948f1057b94
jsing <>
2014-07-25 14:04:51 +0000

    Add missing year to copyright.

--- src/lib/libssl/src/crypto/poly1305/poly1305.h
+++ src/lib/libssl/src/crypto/poly1305/poly1305.h
@@ -1,6 +1,6 @@
-/* $OpenBSD$ */
+/* $OpenBSD: poly1305.h,v 1.2 2014/06/12 15:49:30 deraadt Exp $ */
 /*
- * Copyright (c) Joel Sing <jsing@openbsd.org>
+ * Copyright (c) 2014 Joel Sing <jsing@openbsd.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
