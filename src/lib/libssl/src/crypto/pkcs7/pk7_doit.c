e527e16655f0ab1cca7d6aa8e291cdf1b23787eb
doug <>
2014-07-25 06:05:32 +0000

    BIO_free() returns immediately when the sole input is NULL. Remove unnecessary NULL check.
    ok miod@

--- src/lib/libssl/src/crypto/pkcs7/pk7_doit.c
+++ src/lib/libssl/src/crypto/pkcs7/pk7_doit.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: pk7_doit.c,v 1.27 2014/07/11 08:44:49 jsing Exp $ */
+/* $OpenBSD: pk7_doit.c,v 1.28 2014/07/12 16:03:37 miod Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -133,8 +133,7 @@ PKCS7_bio_add_digest(BIO **pbio, X509_ALGOR *alg)
 	return 1;
 
 err:
-	if (btmp)
-		BIO_free(btmp);
+	BIO_free(btmp);
 	return 0;
 
 }
