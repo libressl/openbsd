daf8356d0c8a055b8ed42d2802ca8e89f4d488f8
bcook <>
2014-12-08 03:54:19 +0000

    Use platform-defined method of printing a pointer.
    Casting a pointer to an unsigned long discards bits on an LLP64 system.
    
    ok deraadt@

--- src/lib/libssl/src/crypto/bio/bio_cb.c
+++ src/lib/libssl/src/crypto/bio/bio_cb.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: bio_cb.c,v 1.14 2014/06/12 15:49:28 deraadt Exp $ */
+/* $OpenBSD: bio_cb.c,v 1.15 2014/07/11 08:44:47 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -76,7 +76,7 @@ BIO_debug_callback(BIO *bio, int cmd, const char *argp, int argi, long argl,
 	if (BIO_CB_RETURN & cmd)
 		r = ret;
 
-	snprintf(buf, sizeof buf, "BIO[%08lX]:", (unsigned long)bio);
+	snprintf(buf, sizeof buf, "BIO[%p]:", bio);
 	p = &(buf[14]);
 	p_maxlen = sizeof buf - 14;
 	switch (cmd) {
