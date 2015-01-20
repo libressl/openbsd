e0944a7b3721cdc93b094339bf8a743af61b68a5
krw <>
2015-01-20 04:41:01 +0000

    Missing $OpenBSD$'s.
    ok deraadt@

--- src/regress/lib/libc/setjmp-fpu/setjmp.c
+++ src/regress/lib/libc/setjmp-fpu/setjmp.c
@@ -1,3 +1,4 @@
+/*	$OpenBSD$	*/
 #define	SETJMP(env, savemask)	setjmp(env)
 #define	LONGJMP(env, val)	longjmp(env, val)
 #define	TEST_SETJMP		test_setjmp
--- src/regress/lib/libc/setjmp-fpu/sigsetjmp.c
+++ src/regress/lib/libc/setjmp-fpu/sigsetjmp.c
@@ -1,3 +1,4 @@
+/*	$OpenBSD$	*/
 #define	SETJMP(env, savemask)	sigsetjmp(env, savemask)
 #define	LONGJMP(env, val)	siglongjmp(env, val)
 #define	TEST_SETJMP		test_sigsetjmp
