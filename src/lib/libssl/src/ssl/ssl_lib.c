530ba0a3863f49cd7567c7dd04b5ebc11ccdefb1
reyk <>
2015-01-22 09:12:57 +0000

    Support CA verification in chroot'ed processes without direct file access to the certificates.  SSL_CTX_load_verify_mem() is a frontend to the new X509_STORE_load_mem() function that allows to load the CA chain from a memory buffer that is holding the PEM-encoded files. This function allows to handle the verification in privsep'ed code.
    Adopted for LibreSSL based on older code from relayd (by pyr@ and myself)
    With feedback and OK bluhm@

--- src/lib/libssl/src/ssl/ssl_lib.c
+++ src/lib/libssl/src/ssl/ssl_lib.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssl_lib.c,v 1.93 2014/12/14 14:34:43 jsing Exp $ */
+/* $OpenBSD: ssl_lib.c,v 1.94 2014/12/15 00:46:53 doug Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -2860,6 +2860,12 @@ SSL_CTX_load_verify_locations(SSL_CTX *ctx, const char *CAfile,
     const char *CApath)
 {
 	return (X509_STORE_load_locations(ctx->cert_store, CAfile, CApath));
+}
+
+int
+SSL_CTX_load_verify_mem(SSL_CTX *ctx, void *buf, int len)
+{
+	return (X509_STORE_load_mem(ctx->cert_store, buf, len));
 }
 
 void
