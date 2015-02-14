9829e66d696eeeb0e17f2b21f4c41182508f74bf
miod <>
2015-02-14 15:21:49 +0000

    Make asn1_ex_i2c() static. ok jsing@

--- src/lib/libssl/src/crypto/asn1/tasn_enc.c
+++ src/lib/libssl/src/crypto/asn1/tasn_enc.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: tasn_enc.c,v 1.13 2014/06/12 15:49:27 deraadt Exp $ */
+/* $OpenBSD: tasn_enc.c,v 1.14 2014/07/11 08:44:47 jsing Exp $ */
 /* Written by Dr Stephen N Henson (steve@openssl.org) for the OpenSSL
  * project 2000.
  */
@@ -71,6 +71,8 @@ static int asn1_template_ex_i2d(ASN1_VALUE **pval, unsigned char **out,
     const ASN1_TEMPLATE *tt, int tag, int aclass);
 static int asn1_item_flags_i2d(ASN1_VALUE *val, unsigned char **out,
     const ASN1_ITEM *it, int flags);
+static int asn1_ex_i2c(ASN1_VALUE **pval, unsigned char *cout, int *putype,
+    const ASN1_ITEM *it);
 
 /* Top level i2d equivalents: the 'ndef' variant instructs the encoder
  * to use indefinite length constructed encoding, where appropriate
@@ -543,7 +545,7 @@ asn1_i2d_ex_primitive(ASN1_VALUE **pval, unsigned char **out,
 
 /* Produce content octets from a structure */
 
-int
+static int
 asn1_ex_i2c(ASN1_VALUE **pval, unsigned char *cout, int *putype,
     const ASN1_ITEM *it)
 {
