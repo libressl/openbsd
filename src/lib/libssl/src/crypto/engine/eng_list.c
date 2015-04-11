0a96ae4fd0cffcab2f0fc5251c696fa996bcba67
deraadt <>
2015-04-11 16:03:21 +0000

    Remove all getenv() calls, especially those wrapped by issetugid(). getenv()'s wrapped by issetugid() are safe, but issetugid() is correct difficult to impliment on many operating systems.  By accident, a grand experiment was run over the last year, where issetugid() returned 1 (the safe value) on a few operating systems.  Noone noticed & complained that certain environment variables were not working....... ok doug beck jsing, discussion with others

--- src/lib/libssl/src/crypto/engine/eng_list.c
+++ src/lib/libssl/src/crypto/engine/eng_list.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: eng_list.c,v 1.16 2015/02/07 13:19:15 doug Exp $ */
+/* $OpenBSD: eng_list.c,v 1.17 2015/02/11 03:19:37 doug Exp $ */
 /* Written by Geoff Thorpe (geoff@geoffthorpe.net) for the OpenSSL
  * project 2000.
  */
@@ -386,12 +386,7 @@ ENGINE_by_id(const char *id)
 		return iterator;
 	/* Prevent infinite recusrion if we're looking for the dynamic engine. */
 	if (strcmp(id, "dynamic")) {
-		if (issetugid() == 0) {
-			load_dir = getenv("OPENSSL_ENGINES");
-			if (load_dir == NULL)
-				load_dir = ENGINESDIR;
-		} else
-			load_dir = ENGINESDIR;
+		load_dir = ENGINESDIR;
 
 		iterator = ENGINE_by_id("dynamic");
 		if (!iterator ||
