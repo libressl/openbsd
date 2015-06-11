4f4effc15e06a5d58d76a183cd7a092ed050d197
jsing <>
2015-06-11 16:09:20 +0000

    MFC: Fix several defects from OpenSSL.
    These include:
    
    CVE-2015-1788 - Malformed ECParameters causes infinite loop
    CVE-2015-1789 - Exploitable out-of-bounds read in X509_cmp_time
    CVE-2015-1792 - CMS verify infinite loop with unknown hash function

--- src/lib/libcrypto/bn/bn_gf2m.c
+++ src/lib/libcrypto/bn/bn_gf2m.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: bn_gf2m.c,v 1.18 2015/02/10 09:50:12 miod Exp $ */
+/* $OpenBSD: bn_gf2m.c,v 1.18.4.1 2015/06/11 16:09:20 jsing Exp $ */
 /* ====================================================================
  * Copyright 2002 Sun Microsystems, Inc. ALL RIGHTS RESERVED.
  *
@@ -745,8 +745,13 @@ BN_GF2m_mod_inv(BIGNUM *r, const BIGNUM *a, const BIGNUM *p, BN_CTX *ctx)
 				ubits--;
 			}
 
-			if (ubits <= BN_BITS2 && udp[0] == 1)
-				break;
+			if (ubits <= BN_BITS2) {
+				/* See if poly was reducible. */
+				if (udp[0] == 0)
+					goto err;
+				if (udp[0] == 1)
+					break;
+			}
 
 			if (ubits < vbits) {
 				i = ubits;
