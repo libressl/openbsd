6d02ae57aea1bd427288916b7c0c31fbb3338bdd
jsing <>
2015-06-20 14:24:49 +0000

    Handle NIST curve names in openssl(1) ecparam.
    From OpenSSL.

--- src/usr.bin/openssl/ecparam.c
+++ src/usr.bin/openssl/ecparam.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ecparam.c,v 1.7 2014/12/28 14:21:42 jsing Exp $ */
+/* $OpenBSD: ecparam.c,v 1.8 2014/12/28 15:48:52 jsing Exp $ */
 /*
  * Written by Nils Larsch for the OpenSSL project.
  */
@@ -360,6 +360,9 @@ ecparam_main(int argc, char **argv)
 			nid = NID_X9_62_prime256v1;
 		} else
 			nid = OBJ_sn2nid(ecparam_config.curve_name);
+
+		if (nid == 0)
+			nid = EC_curve_nist2nid(ecparam_config.curve_name);
 
 		if (nid == 0) {
 			BIO_printf(bio_err, "unknown curve name (%s)\n",
