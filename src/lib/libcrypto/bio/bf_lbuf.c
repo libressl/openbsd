5834c0839ec698d5e2a938c0d4f7ebd52349ec67
miod <>
2015-07-19 18:29:31 +0000

    Drop stupid (int) casts for the arguments of malloc() and friends. This is not 16-bit MS-DOS anymore. ok bcook@ tedu@

--- src/lib/libcrypto/bio/bf_lbuf.c
+++ src/lib/libcrypto/bio/bf_lbuf.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: bf_lbuf.c,v 1.11 2014/06/12 15:49:28 deraadt Exp $ */
+/* $OpenBSD: bf_lbuf.c,v 1.12 2014/07/11 08:44:47 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -280,7 +280,7 @@ linebuffer_ctrl(BIO *b, int cmd, long num, void *ptr)
 		obs = (int)num;
 		p = ctx->obuf;
 		if ((obs > DEFAULT_LINEBUFFER_SIZE) && (obs != ctx->obuf_size)) {
-			p = malloc((int)num);
+			p = malloc(num);
 			if (p == NULL)
 				goto malloc_error;
 		}
