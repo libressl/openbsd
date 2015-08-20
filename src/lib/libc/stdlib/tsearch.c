2f54f2537155a3dd95dda3620a02951cb8924758
deraadt <>
2015-08-20 21:49:29 +0000

    All these files include <stdlib.h>, so do not need to cast malloc/calloc/realloc* returns.

--- src/lib/libc/stdlib/tsearch.c
+++ src/lib/libc/stdlib/tsearch.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tsearch.c,v 1.7 2012/02/06 20:29:54 guenther Exp $	*/
+/*	$OpenBSD: tsearch.c,v 1.8 2014/03/16 18:38:30 guenther Exp $	*/
 
 /*
  * Tree search generalized from Knuth (6.2.2) Algorithm T just like
@@ -40,7 +40,7 @@ tsearch(const void *vkey, void **vrootp,
 	    &(*rootp)->left :		/* T3: follow left branch */
 	    &(*rootp)->right;		/* T4: follow right branch */
     }
-    q = (node *) malloc(sizeof(node));	/* T5: key not found */
+    q = malloc(sizeof(node));	/* T5: key not found */
     if (q != (struct node_t *)0) {	/* make new node */
 	*rootp = q;			/* link new node to old */
 	q->key = key;			/* initialize new node */
