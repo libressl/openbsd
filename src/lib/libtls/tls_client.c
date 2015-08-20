7bad7f980e188dcdadcb1f595f820c1d157525d1
Marko Kreen <markokr@gmail.com>
2015-08-20 21:30:27 +0300

    libtls fix: set ctx as app_data for clients too
    It was set in tls_server() but not tls_client().
    
    Now app_data is always set.  Useful for compat
    callbacks.

--- src/lib/libtls/tls_client.c
+++ src/lib/libtls/tls_client.c
@@ -225,6 +225,8 @@ tls_connect_fds(struct tls *ctx, int fd_read, int fd_write,
 		tls_set_error(ctx, "ssl connection failure");
 		goto err;
 	}
+	SSL_set_app_data(ctx->ssl_conn, ctx);
+
 	if (SSL_set_rfd(ctx->ssl_conn, fd_read) != 1 ||
 	    SSL_set_wfd(ctx->ssl_conn, fd_write) != 1) {
 		tls_set_error(ctx, "ssl file descriptor failure");
