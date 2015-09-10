b0e5d4bc7baed0617f6dd094c650db410af62c78
jsing <>
2015-09-10 16:01:06 +0000

    Correct spelling of OPENSSL_cleanse.

--- src/usr.bin/openssl/dgst.c
+++ src/usr.bin/openssl/dgst.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: dgst.c,v 1.3 2015/07/20 18:23:52 rpointel Exp $ */
+/* $OpenBSD: dgst.c,v 1.4 2015/08/22 16:36:05 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -457,7 +457,7 @@ mac_end:
 
 end:
 	if (buf != NULL) {
-		OPENSSL_cleanse(buf, BUFSIZE);
+		explicit_bzero(buf, BUFSIZE);
 		free(buf);
 	}
 	if (in != NULL)
