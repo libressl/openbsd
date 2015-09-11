a2c3a405cabd28f2e1d22464ef2b57791061a09f
beck <>
2015-09-11 08:31:26 +0000

    actually set return value to 0 on success. ok jsing@ who wears the cone of shame.

--- src/lib/libtls/tls_server.c
+++ src/lib/libtls/tls_server.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: tls_server.c,v 1.14 2015/09/10 09:10:42 jsing Exp $ */
+/* $OpenBSD: tls_server.c,v 1.15 2015/09/10 10:14:20 jsing Exp $ */
 /*
  * Copyright (c) 2014 Joel Sing <jsing@openbsd.org>
  *
@@ -173,6 +173,7 @@ tls_handshake_server(struct tls *ctx)
 	}
 
 	ctx->state |= TLS_HANDSHAKE_COMPLETE;
+	rv = 0;
 
  err:
 	return (rv);
