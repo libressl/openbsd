e54742cc5b8606f1e15bbb60ab88062989475b31
bcook <>
2015-09-11 14:30:23 +0000

    Remove engine command and parameters from openssl(1).
    We do not have any builtin or dynamic engines, meaning openssl(1) has
    no way to use the engine command or parameters at all.
    
    ok jsing@

--- src/usr.bin/openssl/progs.h
+++ src/usr.bin/openssl/progs.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: progs.h,v 1.5 2015/08/19 18:25:31 deraadt Exp $ */
+/* $OpenBSD: progs.h,v 1.6 2015/08/22 16:36:05 jsing Exp $ */
 /* Public domain */
 
 int asn1parse_main(int argc, char **argv);
@@ -16,7 +16,6 @@ int dsaparam_main(int argc, char **argv);
 int ec_main(int argc, char **argv);
 int ecparam_main(int argc, char **argv);
 int enc_main(int argc, char **argv);
-int engine_main(int argc, char **argv);
 int errstr_main(int argc, char **argv);
 int gendh_main(int argc, char **argv);
 int gendsa_main(int argc, char **argv);
