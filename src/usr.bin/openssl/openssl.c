15a455a9d59e30e6957007df0cc109501b6e1c0f
lteo <>
2015-09-12 19:34:07 +0000

    Nuke SSLEAY_CONF -- a backwards compatibility environment variable that has been superseded by OPENSSL_CONF and discouraged from use for almost 16 years.
    "Definately ok" jsing@
    "burn it" deraadt@
    "Kill it with fire" miod@
    "KILL IT WITH FIRE!!! BURN!!!!" beck@

--- src/usr.bin/openssl/openssl.c
+++ src/usr.bin/openssl/openssl.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: openssl.c,v 1.7 2015/09/11 14:30:23 bcook Exp $ */
+/* $OpenBSD: openssl.c,v 1.8 2015/09/11 20:55:59 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -455,8 +455,6 @@ main(int argc, char **argv)
 
 	/* Lets load up our environment a little */
 	p = getenv("OPENSSL_CONF");
-	if (p == NULL)
-		p = getenv("SSLEAY_CONF");
 	if (p == NULL) {
 		p = to_free = make_config_name();
 		if (p == NULL) {
