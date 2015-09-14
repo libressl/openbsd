69f72268fa77815e62a2b52d2a3bd283787238fc
jsing <>
2015-09-14 16:16:38 +0000

    Provide tls_config_insecure_noverifytime() in order to be able to disable certificate validity checking.
    ok beck@

--- src/lib/libtls/tls_internal.h
+++ src/lib/libtls/tls_internal.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: tls_internal.h,v 1.22 2015/09/13 10:32:46 beck Exp $ */
+/* $OpenBSD: tls_internal.h,v 1.23 2015/09/14 12:29:16 jsing Exp $ */
 /*
  * Copyright (c) 2014 Jeremie Courreges-Anglas <jca@openbsd.org>
  * Copyright (c) 2014 Joel Sing <jsing@openbsd.org>
@@ -46,6 +46,7 @@ struct tls_config {
 	int verify_client;
 	int verify_depth;
 	int verify_name;
+	int verify_time;
 };
 
 struct tls_conninfo {
