24cfcca5e087d1cc8f4dc76dfd102661c73b572b
guenther <>
2015-09-14 13:30:17 +0000

    Only two of the *rand48.c files need <math.h>, so just #include it in them

--- src/lib/libc/stdlib/erand48.c
+++ src/lib/libc/stdlib/erand48.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: erand48.c,v 1.3 2005/08/08 08:05:36 espie Exp $ */
+/*	$OpenBSD: erand48.c,v 1.4 2015/09/13 08:31:47 guenther Exp $ */
 /*
  * Copyright (c) 1993 Martin Birgmeier
  * All rights reserved.
@@ -12,6 +12,7 @@
  * to anyone/anything when using this software.
  */
 
+#include <math.h>
 #include "rand48.h"
 
 double
