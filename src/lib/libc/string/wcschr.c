43b229266e7636d30be34d1eb609ec85c0bf1516
guenther <>
2015-10-01 02:32:07 +0000

    Eliminate the last of the LINTEDn and PRINTFLIKEn comments.  In one case, by deleting some useless '& of an array' we also eliminate the need for the casts which prompted the original lint warnings
    ok deraadt@

--- src/lib/libc/string/wcschr.c
+++ src/lib/libc/string/wcschr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: wcschr.c,v 1.4 2008/08/23 05:34:36 djm Exp $	*/
+/*	$OpenBSD: wcschr.c,v 1.5 2015/09/12 16:23:14 guenther Exp $	*/
 /*	$NetBSD: wcschr.c,v 1.2 2001/01/03 14:29:36 lukem Exp $	*/
 
 /*-
@@ -39,7 +39,6 @@ wcschr(const wchar_t *s, wchar_t c)
 	p = s;
 	for (;;) {
 		if (*p == c) {
-			/* LINTED interface specification */
 			return (wchar_t *)p;
 		}
 		if (!*p)
