460bf2050fd219822f2393389f36ec87eef06518
jsing <>
2015-10-16 15:09:28 +0000

    Remove pointless uses of DECLARE_ASN1_ENCODE_FUNCTIONS_const.
    DECLARE_ASN1_FUNCTIONS_const already includes this macro so using both
    means we end up with duplicate function prototypes and externs.

--- src/lib/libssl/src/crypto/ecdsa/ecs_asn1.c
+++ src/lib/libssl/src/crypto/ecdsa/ecs_asn1.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ecs_asn1.c,v 1.4 2015/02/10 04:01:26 jsing Exp $ */
+/* $OpenBSD: ecs_asn1.c,v 1.5 2015/07/24 15:51:49 jsing Exp $ */
 /* ====================================================================
  * Copyright (c) 2000-2002 The OpenSSL Project.  All rights reserved.
  *
@@ -85,7 +85,6 @@ const ASN1_ITEM ECDSA_SIG_it = {
 };
 
 DECLARE_ASN1_FUNCTIONS_const(ECDSA_SIG)
-DECLARE_ASN1_ENCODE_FUNCTIONS_const(ECDSA_SIG, ECDSA_SIG)
 
 ECDSA_SIG *
 d2i_ECDSA_SIG(ECDSA_SIG **a, const unsigned char **in, long len)
