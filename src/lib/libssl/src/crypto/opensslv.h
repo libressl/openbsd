6b46114ee740161fa90a892a435ab4007d64cd65
bcook <>
2016-05-29 20:53:29 +0000

    bump to 2.3.5

--- src/lib/libssl/src/crypto/opensslv.h
+++ src/lib/libssl/src/crypto/opensslv.h
@@ -1,10 +1,10 @@
-/* $OpenBSD: opensslv.h,v 1.33.2.2 2016/05/03 13:41:17 bcook Exp $ */
+/* $OpenBSD: opensslv.h,v 1.33.2.3 2016/05/29 20:53:29 bcook Exp $ */
 #ifndef HEADER_OPENSSLV_H
 #define HEADER_OPENSSLV_H
 
 /* These will change with each release of LibreSSL-portable */
-#define LIBRESSL_VERSION_NUMBER	0x2030300fL
-#define LIBRESSL_VERSION_TEXT	"LibreSSL 2.3.4"
+#define LIBRESSL_VERSION_NUMBER	0x2030500fL
+#define LIBRESSL_VERSION_TEXT	"LibreSSL 2.3.5"
 
 /* These will never change */
 #define OPENSSL_VERSION_NUMBER	0x20000000L
