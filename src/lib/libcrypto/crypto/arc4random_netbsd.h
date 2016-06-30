00aeb06221ce8b182f52a44eceda20f33ef7f9f3
bcook <>
2016-06-30 12:19:51 +0000

    Tighten behavior of _rs_allocate failure for portable arc4random implementations.
    In the event of a failure in _rs_allocate for rsx, we still have a reference to
    freed memory for rs on return. Not a huge deal since we subsequently abort in
    _rs_init, but it looks strange on its own.
    
    ok deraadt@

--- src/lib/libcrypto/crypto/arc4random_netbsd.h
+++ src/lib/libcrypto/crypto/arc4random_netbsd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: arc4random_netbsd.h,v 1.1 2015/01/19 20:21:40 bcook Exp $	*/
+/*	$OpenBSD: arc4random_netbsd.h,v 1.2 2015/09/11 11:52:55 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1996, David Mazieres <dm@uun.org>
@@ -78,6 +78,7 @@ _rs_allocate(struct _rs **rsp, struct _rsx **rsxp)
 	if ((*rsxp = mmap(NULL, sizeof(**rsxp), PROT_READ|PROT_WRITE,
 	    MAP_ANON|MAP_PRIVATE, -1, 0)) == MAP_FAILED) {
 		munmap(*rsp, sizeof(**rsp));
+		*rsp = NULL;
 		return (-1);
 	}
 
