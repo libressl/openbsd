27570b8d2eae6aae7b2773d379f434b88d7a20c2
deraadt <>
2016-09-05 10:45:19 +0000

    remove a cms leftover

--- src/usr.bin/openssl/progs.h
+++ src/usr.bin/openssl/progs.h
@@ -1,11 +1,10 @@
-/* $OpenBSD: progs.h,v 1.6 2015/08/22 16:36:05 jsing Exp $ */
+/* $OpenBSD: progs.h,v 1.7 2015/09/11 14:30:23 bcook Exp $ */
 /* Public domain */
 
 int asn1parse_main(int argc, char **argv);
 int ca_main(int argc, char **argv);
 int certhash_main(int argc, char **argv);
 int ciphers_main(int argc, char **argv);
-int cms_main(int argc, char **argv);
 int crl2pkcs7_main(int argc, char **argv);
 int crl_main(int argc, char **argv);
 int dgst_main(int argc, char **argv);
