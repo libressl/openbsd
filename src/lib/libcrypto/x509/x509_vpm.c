12a8b93ac013f6d739c3d7146727148b686ac1a1
beck <>
2016-11-06 10:17:49 +0000

    Commit a reminder that the default is not the default. This needs to be revisited. ok jsing@

--- src/lib/libcrypto/x509/x509_vpm.c
+++ src/lib/libcrypto/x509/x509_vpm.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: x509_vpm.c,v 1.12 2016/11/05 20:14:59 beck Exp $ */
+/* $OpenBSD: x509_vpm.c,v 1.13 2016/11/06 09:28:25 bcook Exp $ */
 /* Written by Dr Stephen N Henson (steve@openssl.org) for the OpenSSL
  * project 2004.
  */
@@ -575,6 +575,7 @@ static const X509_VERIFY_PARAM default_table[] = {
 	{
 		.name = "default",
 		.depth = 100,
+		.trust = 0,  /* XXX This is not the default trust value */
 		.id = vpm_empty_id
 	},
 	{
