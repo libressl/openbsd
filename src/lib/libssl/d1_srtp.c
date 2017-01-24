4de6705ad585f4626baf7b60cf84f1258cda9a17
jsing <>
2017-01-24 15:04:12 +0000

    sk_free() checks for NULL so do not bother doing it from the callers.

--- src/lib/libssl/d1_srtp.c
+++ src/lib/libssl/d1_srtp.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: d1_srtp.c,v 1.16 2017/01/23 05:13:02 jsing Exp $ */
+/* $OpenBSD: d1_srtp.c,v 1.17 2017/01/23 06:45:30 beck Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -371,8 +371,7 @@ ssl_parse_clienthello_use_srtp_ext(SSL *s, const unsigned char *d, int len,
 	ret = 0;
 
 done:
-	if (clnt)
-		sk_SRTP_PROTECTION_PROFILE_free(clnt);
+	sk_SRTP_PROTECTION_PROFILE_free(clnt);
 
 	return ret;
 }
--- src/lib/libssl/s3_clnt.c
+++ src/lib/libssl/s3_clnt.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: s3_clnt.c,v 1.171 2017/01/24 01:39:13 jsing Exp $ */
+/* $OpenBSD: s3_clnt.c,v 1.172 2017/01/24 14:57:31 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -1099,6 +1099,7 @@ err:
 	EVP_PKEY_free(pkey);
 	X509_free(x);
 	sk_X509_pop_free(sk, X509_free);
+
 	return (ret);
 }
 
