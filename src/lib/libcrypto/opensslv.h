5044e73fda29e14aa487421b75abb668af034376
bcook <>
2018-12-15 15:13:23 +0000

    bump for LibreSSL 2.7.5

--- src/lib/libcrypto/opensslv.h
+++ src/lib/libcrypto/opensslv.h
@@ -1,11 +1,11 @@
-/* $OpenBSD: opensslv.h,v 1.46.2.2 2018/06/13 14:54:17 bcook Exp $ */
+/* $OpenBSD: opensslv.h,v 1.46.2.3 2018/12/15 15:13:23 bcook Exp $ */
 #ifndef HEADER_OPENSSLV_H
 #define HEADER_OPENSSLV_H
 
 /* These will change with each release of LibreSSL-portable */
-#define LIBRESSL_VERSION_NUMBER 0x2070400fL
+#define LIBRESSL_VERSION_NUMBER 0x2070500fL
 /*                                    ^ Patch starts here   */
-#define LIBRESSL_VERSION_TEXT   "LibreSSL 2.7.4"
+#define LIBRESSL_VERSION_TEXT   "LibreSSL 2.7.5"
 
 /* These will never change */
 #define OPENSSL_VERSION_NUMBER	0x20000000L
