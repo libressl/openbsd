f5cdaf4e4d7b589f05e0f2c5368a3833d0d5b863
bcook <>
2019-06-10 01:52:16 +0000

    bump to LibreSSL 2.9.3

--- src/lib/libcrypto/opensslv.h
+++ src/lib/libcrypto/opensslv.h
@@ -1,11 +1,11 @@
-/* $OpenBSD: opensslv.h,v 1.51.2.1 2019/05/13 12:05:04 bcook Exp $ */
+/* $OpenBSD: opensslv.h,v 1.51.2.2 2019/06/10 01:52:16 bcook Exp $ */
 #ifndef HEADER_OPENSSLV_H
 #define HEADER_OPENSSLV_H
 
 /* These will change with each release of LibreSSL-portable */
-#define LIBRESSL_VERSION_NUMBER 0x2090200fL
+#define LIBRESSL_VERSION_NUMBER 0x2090300fL
 /*                                    ^ Patch starts here   */
-#define LIBRESSL_VERSION_TEXT   "LibreSSL 2.9.2"
+#define LIBRESSL_VERSION_TEXT   "LibreSSL 2.9.3"
 
 /* These will never change */
 #define OPENSSL_VERSION_NUMBER	0x20000000L
